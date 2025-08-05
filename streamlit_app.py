8a7fa78c3985498f1aa55530a93f116d8c1b1104
datavislearningacct <datavislearningacct@gmail.com>
2025-08-05 15:03:38 -0400

    Definitions at scatter

--- streamlit_app.py
+++ streamlit_app.py
@@ -191,16 +191,22 @@ with col2:
     
     # Select X-axis metric
     pell_footer = "" # lank unless Pell Grants is selected
+    finaid_footer = ""
+    fedloan_footer = ""
     if bar_dimension == "Any Financial Aid":
         x_metric = ["Percent_financial_aid", "Financial Aid (%)"]
+        finaid_footer = "*Any financial aid includes grants, loans, assistantships, scholarships, waivers, discounts, benefits, and other monies provided to students to meet expenses."
     elif bar_dimension == "Pell Grants*":
         x_metric = ["Percent_Pell_grants", "Pell Grants (%)"]
-        pell_footer = "*A Pell Grant is a federal grant awarded to college students meeting the criteria for financial neediness."
+        pell_footer = "*A Pell Grant is a federal grant that provides grant assistance to eligible students with demonstrated financial need to help meet education expenses."
     elif bar_dimension == "Federal Loans":
         x_metric = ["Percent_federal_loans","Federal Loans (%)"]
-
+        fedloan_footer = "*Federal loans are monies borrowed via the federal government that must be repaid for which the student is the designated borrower."
+        
     # Add conditional notation about Pell Grants
     st.markdown(pell_footer)
+    st.markdown(finaid_footer)
+    st.markdown(fedloan_footer)
     
     # Create scatterplot
     scatter = alt.Chart(ipeds_refiltered).mark_circle(color='#2878b7').encode(
